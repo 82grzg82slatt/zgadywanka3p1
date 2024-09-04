b3aa751e046eeebbbb977403c9885b921ad04469
82grzg82slatt <pizzapan65@gmail.com>
2024-09-04 15:22:25 +0200

    pusty projekt

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    //nazwa pliku i nazwa klasy muszą być takie same
+    //nazwy klas powinny być z dużej litery bo tak karze obyczaj
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+//pushowanie na githuba
