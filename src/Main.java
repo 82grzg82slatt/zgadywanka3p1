407addd2f60fac196506b4cd1154fe5a945a2b65
82grzg82slatt <pizzapan65@gmail.com>
2024-09-04 15:45:31 +0200

    dodatkowy komunikat

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     //nazwy klas powinny być z dużej litery bo tak karze obyczaj
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Witamy na lekcjach programowania");
     }
 }
 //pushowanie na githuba
